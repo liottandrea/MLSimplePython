d8a0813cc0eb007bb915035cd3a97cbf6b12c7e9
Andrea Liotta <liottandrea@users.noreply.github.com>
2018-02-05 21:02:10 +0000

    add function io in different file

--- 01_regression.py
+++ 01_regression.py
@@ -2,6 +2,10 @@
 
 # Regression Template
 
+# install a module in IPython in Spyder
+# type in the console
+# !pip install [name of the module]
+
 #%% ENV
 
 # Importing the libraries
@@ -11,29 +15,22 @@ import matplotlib.pyplot as plt
 # data partition
 from sklearn.model_selection import train_test_split
 # Encoding categorical data
-#from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+# from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 # for linear regression
 from sklearn.linear_model import LinearRegression
 # linear regression
-import statsmodels.formula.api as sm
+# import statsmodels.formula.api as sm
 # poly variables
 from sklearn.preprocessing import PolynomialFeatures
-
-# functions
-def csv2dfs(csv_file,x_cols,y_col):
-    # read csv
-    dataset = pd.read_csv(csv_file)
-    # divide x and y
-    X = dataset[x_cols]
-    y = dataset[y_col]
-    # force to datafram in case only one column
-    if isinstance(X, pd.Series):
-        X = X.to_frame()
-    if isinstance(y, pd.Series):
-        y = y.to_frame()
-    # return
-    return X,y
-
+# scaling
+from sklearn.preprocessing import StandardScaler
+# pandas and sklearn working together
+# https://github.com/scikit-learn-contrib/sklearn-pandas
+# from sklearn_pandas import DataFrameMapper
+# SVR
+from sklearn.svm import SVR
+# extra functions
+import dataInOut as myio
 
 #%% SETTING
 
@@ -45,7 +42,8 @@ test_size = 0.2
 # IN
 which_input = 'startups'
 which_input = 'salary_linear'
-
+which_input = 'salary_poly'
+which_input = 'salary_svm'
 
 # 50_Startups - multiple linear
 if which_input == 'startups': 
@@ -54,33 +52,77 @@ if which_input == 'startups':
             'y_col':'Profit',
             'X_cols':['R&D Spend', 'Administration',
                       'Marketing Spend', 'State'],
-            'dummy_cols':'State'}
+            'dummy_cols':'State',
+            'poly_degree': np.nan}
 
 # Salary_Data - simple linear
 if which_input == 'salary_linear': 
     input_setting = {
             'csv_file':'data/Salary_Data.csv',
             'y_col':'Salary',
-            'X_cols':['YearsExperience'],
-            'dummy_cols':np.nan}
+            'X_cols':'YearsExperience',
+            'dummy_cols': np.nan,
+            'poly_degree': np.nan}
+
+# 'Position_Salaries.csv' for poly
+if which_input == 'salary_poly': 
+    input_setting = {
+            'csv_file':'data/Position_Salaries.csv',
+            'y_col':'Salary',
+            'X_cols':'Level',
+            'dummy_cols':np.nan,
+            'poly_degree': 4}
+
+# 'Position_Salaries.csv' for svm
+if which_input == 'salary_svm': 
+    input_setting = {
+            'csv_file':'data/Position_Salaries.csv',
+            'y_col':'Salary',
+            'X_cols':'Level',
+            'feature_scaling':True,
+            'poly_degree': np.nan}
+
 
 #%% INPUT
 
 # from csv to two datasets
-X, y = csv2dfs(input_setting['csv_file'],
-                    input_setting['X_cols'],
-                    input_setting['y_col'])
+X, y = myio.csv_df2Xy(
+        input_setting['csv_file'],
+        input_setting['X_cols'],
+        input_setting['y_col'])
 
 
 #%% PREPROCESSING
 
+# Feature Scaling
+if pd.notnull(input_setting['feature_scaling']):
+    sc_X = StandardScaler()
+    sc_y = StandardScaler()
+    X = sc_X.fit_transform(X)
+    y = sc_y.fit_transform(y)
+
+
 # DUMMY VARIABLES
-if isinstance(input_setting['dummy_cols'],list):
+if pd.notnull(input_setting['dummy_cols']):
     X = pd.get_dummies(
             X,
             columns = [input_setting['dummy_cols']],
             drop_first = True) # Avoiding the Dummy Variable Trap
 
+# POLYNOMIAL
+if pd.notnull(input_setting['poly_degree']):
+    # set polynomial
+    poly_transform = PolynomialFeatures(
+            degree = input_setting['poly_degree'],
+            include_bias = False)
+    # fit it
+    poly_transform.fit_transform(X)
+    # put in a df
+    X = pd.DataFrame(
+            data = poly_transform.fit_transform(X),
+            columns = poly_transform.get_feature_names(X.columns),
+            index = X.index)
+    
 # INTERCEPT
 X = pd.DataFrame(
         data = PolynomialFeatures(degree = 1).fit_transform(X),
@@ -97,14 +139,22 @@ X_train, X_test, y_train, y_test = train_test_split(
         random_state = seed_random)
 
 
-#%% LINEAR REGRESSION
+#%% REGRESSION simple, multi, poly
 
 # FIT
 # Fitting Multiple Linear Regression to the Training set
 regressor = LinearRegression(fit_intercept = True)
 regressor.fit(X_train, y_train)
 
-# PREDICT
+
+#%% SV REGRESSION
+
+# FIT
+regressor = SVR(kernel = 'rbf')
+regressor.fit(X, y)
+
+
+#%% PREDICT
 # fitted values on train set
 y_train_pred = pd.DataFrame(data = regressor.predict(X_train),
                       columns = list(y_train),
@@ -114,11 +164,6 @@ y_train_pred = pd.DataFrame(data = regressor.predict(X_train),
 y_test_pred = pd.DataFrame(data = regressor.predict(X_test),
                       columns = list(y_train),
                       index = X_test.index)
-
-
-#%% POLYNOMIAL REGRESSION
-
-
 
 
 #%% VISUALISE
@@ -138,6 +183,21 @@ plt.plot(X_train[input_setting['X_cols']].values, y_train_pred.values, color = '
 plt.scatter(X_test[input_setting['X_cols']].values, y_test_pred.values, color = 'orange')
 plt.title('Salary vs Experience (Test set)')
 plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+
+# Polynomial Regression 
+## Visualising the Polynomial Regression results
+## (for higher resolution and smoother curve)
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid,
+         regressor.predict(poly_transform.fit_transform(X_grid)),
+         color = 'blue')
+plt.title('Truth or Bluff (Polynomial Regression)')
+plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
 
---
+++ dataInOut.py
@@ -0,0 +1,52 @@
+#%% DESCRIPTION
+# data InOut
+"""
+collection of function to handle csv(s) and db table both in and out
+"""
+
+#%% ENV
+import psycopg2
+import pandas as pd
+import pandas.io.sql as psql
+
+
+#%% FUNCTIONS
+
+# Connect to the db
+def db_connect2db():
+    # define our connection string
+    conn_string = "host='localhost' dbname='mydb' user='postgres' password='secret'"
+    # print the connection string we will use to connect
+    print ("Connecting to database\n%s" % conn_string)
+    # get a connection, if a connect cannot be made an exception will be raised here
+    conn = psycopg2.connect(conn_string)
+    return conn
+
+# select a table
+def db_table2df(conn,table):
+    return psql.read_sql_query("select * from %s" % table, conn)
+
+
+# functions
+def csv_df2Xy(csv_file,x_cols,y_col): 
+    # read csv
+    dataset = pd.read_csv(csv_file)
+    # divide x and y
+    X = dataset[x_cols]
+    y = dataset[y_col]
+    # force to dataframe in case only one column
+    if isinstance(X, pd.Series):
+        X = X.to_frame()
+    if isinstance(y, pd.Series):
+        y = y.to_frame()
+    # return
+    return X,y
+ 
+#%% EXAMPLES
+    
+"""
+# conn to db
+db_conn = db_connect2db()
+# download table
+table = db_table2df(db_conn,"my_table")
+"""
